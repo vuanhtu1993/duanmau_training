74e995d08d2a63e2f8a723da222e5803767462c8
tuva9 <tuva9@fpt.edu.vn>
2023-09-25 10:37:25 +0700

    Buổi 3: PDO

--- models/pdo.php
+++ models/pdo.php
@@ -7,7 +7,7 @@ function pdo_get_connection(){
     try {
         // connect
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); //3306
-        $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
@@ -15,33 +15,14 @@ function pdo_get_connection(){
 }   
 
 function pdo_query($sql) {
-    $sql_args = array_slice(func_get_args(), 1);
     try {
         $connect = pdo_get_connection();
-        $stmt = $connect->prepare($sql);
-        $stmt->execute($sql_args);
-        $rows = $stmt->fetchAll();
+        $statement = $connect->prepare($sql);
+        $statement->execute();
+        $rows = $statement->fetchAll();
         return $rows;
     } catch(PDOException $e) {
         throw $e;
-    } finally {
-        unset($connect);
-    }
-}   
-
-function pdo_query_one($sql) {
-    $sql_args = array_slice(func_get_args(), 1);
-    try {
-        $connect = pdo_get_connection();
-        $stmt = $connect->prepare($sql);
-        $stmt->execute($sql_args);
-        $rows = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $rows;
-    } catch(PDOException $e) {
-        throw $e;
-    } finally {
-        unset($connect);
     }
 }
-
 ?>
--- models/sanpham.php
+++ models/sanpham.php
@@ -1,7 +1,9 @@
 <?php
+// MODEL SANPHAM
 function loadall_sanpham() {
-    $sql = "select * from sanpham where 1";
+    $sql = "select * from sanpham where 1 order by id desc";
     $list_sanpham = pdo_query($sql);
     return $list_sanpham;
 }
+
 ?>
